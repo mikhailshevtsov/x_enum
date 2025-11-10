25dfd49af870135976901abc760d0c4274d8982b
mikhailshevtsov <mikhail.shevtsoff@gmail.com>
2025-11-10 17:14:45 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,93 @@
+# MyLib
+Lightweight zero-cost cross-platform header-only C++ library for introducing eflection capabilities to native C++ enums
+
+## Features
+- Only one header, no dependencies
+- C++20-compatible
+- Cross-platform
+- Introduces reflection to existing native С++-enums
+- Based on X macro technology, does not make any use of template magic
+- Supports any native С++ enums without restrictions on their size
+
+## Installation
+Simply copy `x_enum.hpp` into your project or link it via CMake:
+
+```cmake
+# CMake
+include(FetchContent)
+FetchContent_Declare(
+  x_enum
+  GIT_REPOSITORY https://github.com/mikhailshevtsov/x_enum.git
+  GIT_TAG main
+)
+FetchContent_MakeAvailable(x_enum)
+target_link_libraries(your_project PRIVATE x_enum)
+```
+
+## Example of usage
+```cpp
+#include "x_enum.hpp"
+#include <iostream>
+
+// Macro description of enum Color
+#define X_ENUM_Color(X) \
+    X(Red, 42) \
+    X(Green) \
+    X(Blue)
+
+// Reflective enum inside global namespace
+X_ENUM(Color)
+
+// Refelective enum inside namespace
+namespace YourNamespace
+{
+namespace YourNestedNamespace
+{
+X_ENUM(Color)
+}
+}
+
+// Reflective enum inside struct or class
+struct YourStruct
+{
+    X_ENUM_IN_CLASS(Color)
+};
+
+// Regular enum inside namespace (it could be inside some third-party library)
+namespace ExternalNamespace
+{
+namespace ExternalNestedNamespace
+{
+enum class Color { Red = 42, Green, Blue };
+}
+}
+
+// Regular enum inside struct or class 
+struct ExternalStruct
+{
+    enum class Color { Red = 42, Green, Blue };
+};
+
+// Adding reflection capabilites to existing enums
+X_DESCRIBE_ENUM_EXTERNAL(Color, ExternalNamespace::ExternalNestedNamespace)
+X_DESCRIBE_ENUM_EXTERNAL(Color, ExternalStruct)
+
+int main()
+{
+    using EnumT = Color;
+    using ValueT = x_enum::underlying_type_t<EnumT>;
+
+    std::cout << x_enum::enum_name<EnumT>() << " enum has " << x_enum::enum_size<EnumT>() << " enumers:\n";
+    for (EnumT enumer : x_enum::enum_enumers<EnumT>())
+    {
+        std::cout << "  ";
+        std::cout << x_enum::to_string(enumer) << " = ";
+        std::cout << static_cast<ValueT>(enumer) << "\n";
+    }
+
+    return 0;
+}
+```
+
+## License
+MIT License © 2025 Mikhail Shevtsov
--- x_enum.hpp
+++ x_enum.hpp
@@ -5,7 +5,6 @@
 #include <array>
 #include <stdexcept>
 #include <string_view>
-#include <optional>
 
 #define X_FOREACH(ENUM_NAME) X_ENUM_##ENUM_NAME
 
@@ -13,9 +12,8 @@
 #define X_ENUMER(NAME, ...) enum_type::NAME,
 #define X_ENUMER_NAME(NAME, ...) #NAME,
 #define X_ENUMER_VALUE(NAME, ...) static_cast<underlying_type>(enum_type::NAME),
-#define X_ENUMER_TO_STRING(NAME, ...) case enum_type::NAME: return #NAME; break;
 
-#define X_DEFINE_ENUM_IMPL(ENUM_NAME, UNSCOPED, ...) enum UNSCOPED ENUM_NAME __VA_OPT__(:) __VA_ARGS__ { X_FOREACH(ENUM_NAME)(X_DEFINE_ENUMER) }
+#define X_DEFINE_ENUM_IMPL(ENUM_NAME, SCOPE, ...) enum SCOPE ENUM_NAME __VA_OPT__(:) __VA_ARGS__ { X_FOREACH(ENUM_NAME)(X_DEFINE_ENUMER) }
 #define X_DEFINE_ENUM(ENUM_NAME, ...) X_DEFINE_ENUM_IMPL(ENUM_NAME, class, __VA_ARGS__)
 #define X_DEFINE_ENUM_UNSCOPED(ENUM_NAME, ...) X_DEFINE_ENUM_IMPL(ENUM_NAME, , __VA_ARGS__)
 
@@ -23,22 +21,16 @@
     using underlying_type = std::underlying_type_t<enum_type>; \
     \
 private: \
-    static constexpr enum_type _array[] { X_FOREACH(ENUM_NAME)(X_ENUMER) }; \
+    static constexpr enum_type _enumers[] { X_FOREACH(ENUM_NAME)(X_ENUMER) }; \
     static constexpr const char* _names[] { X_FOREACH(ENUM_NAME)(X_ENUMER_NAME) }; \
     static constexpr underlying_type _values[] { X_FOREACH(ENUM_NAME)(X_ENUMER_VALUE) }; \
     \
 public: \
     static constexpr const char* name() noexcept { return #ENUM_NAME; } \
-    static constexpr const auto& array() noexcept { return _array; } \
+    static constexpr const auto& enumers() noexcept { return _enumers; } \
     static constexpr const auto& names() noexcept { return _names; } \
     static constexpr const auto& values() noexcept { return _values; } \
     static constexpr std::size_t size() noexcept { return sizeof(_names) / sizeof(const char*); } \
-    \
-    static constexpr const char* to_string(enum_type enumer) noexcept \
-    { \
-        switch (enumer) { X_FOREACH(ENUM_NAME)(X_ENUMER_TO_STRING) } \
-        return nullptr; \
-    } \
 
 #define X_ENUM_TRAITS(ENUM_NAME) X_##ENUM_NAME##_traits
 
@@ -50,7 +42,7 @@ public: \
     X_DESCRIBE_ENUM_BODY(ENUM_NAME) \
 }; \
 IN_CLASS constexpr X_ENUM_TRAITS(ENUM_NAME) x_enum_traits(std::type_identity_t<ENUM_NAME>) noexcept { return {}; } \
-static_assert(x::is_enum_v<ENUM_NAME>, "using X_ENUM inside the class scope");
+static_assert(x_enum::is_enum_v<ENUM_NAME>, "using X_ENUM inside the class scope");
 
 #define X_DESCRIBE_ENUM(ENUM_NAME) X_DESCRIBE_ENUM_IMPL(ENUM_NAME,)
 #define X_DESCRIBE_ENUM_IN_CLASS(ENUM_NAME) X_DESCRIBE_ENUM_IMPL(ENUM_NAME, friend)
@@ -73,17 +65,17 @@ X_DESCRIBE_ENUM_IN_CLASS(ENUM_NAME)
 
 #define X_DESCRIBE_ENUM_EXTERNAL(ENUM_NAME, ...) \
 template <typename T> \
-struct x::is_enum<__VA_ARGS__::ENUM_NAME, T> : std::true_type {}; \
+struct x_enum::is_enum<__VA_ARGS__::ENUM_NAME, T> : std::true_type {}; \
 \
 template <> \
-class x::enum_traits<__VA_ARGS__::ENUM_NAME> \
+class x_enum::enum_traits<__VA_ARGS__::ENUM_NAME> \
 { \
 public: \
     using enum_type = __VA_ARGS__::ENUM_NAME; \
     X_DESCRIBE_ENUM_BODY(ENUM_NAME) \
 };
 
-namespace x
+namespace x_enum
 {
 
 template <typename...>
@@ -111,7 +103,7 @@ template <enumeration EnumT>
 constexpr const char* enum_name() noexcept { return enum_traits<EnumT>::name(); }
 
 template <enumeration EnumT>
-constexpr const auto& enum_array() noexcept { return enum_traits<EnumT>::array(); }
+constexpr const auto& enum_enumers() noexcept { return enum_traits<EnumT>::enumers(); }
 
 template <enumeration EnumT>
 constexpr const auto& enum_names() noexcept { return enum_traits<EnumT>::names(); }
@@ -123,28 +115,14 @@ template <enumeration EnumT>
 constexpr std::size_t enum_size() noexcept { return enum_traits<EnumT>::size(); }
 
 template <enumeration EnumT>
-constexpr std::size_t to_index(EnumT enumer) noexcept
+constexpr const char* to_string(EnumT enumer)
 {
-    constexpr const auto& array = enum_array<EnumT>();
+    constexpr const auto& enumers = enum_enumers<EnumT>();
     constexpr std::size_t size = enum_size<EnumT>();
     for (std::size_t i = 0; i < size; ++i)
-        if (enumer == array[i])
-            return i;
-    return size;
-}
-
-template <enumeration EnumT>
-constexpr const char* to_string(EnumT enumer) noexcept { return enum_traits<EnumT>::to_string(enumer); }
-
-template <enumeration EnumT>
-constexpr underlying_type_t<EnumT> to_value(EnumT enumer) noexcept { return static_cast<underlying_type_t<EnumT>>(enumer); }
-
-template <enumeration EnumT>
-constexpr EnumT from_index(std::size_t index)
-{
-    if (index < enum_size<EnumT>())
-        return enum_array<EnumT>()[index];
-    throw std::out_of_range("enum index is out of range");
+        if (enumers[i] == enumer)
+            return enum_names<EnumT>()[i];
+    throw std::out_of_range("enum value is out of range");
 }
 
 template <enumeration EnumT>
@@ -154,49 +132,8 @@ constexpr EnumT from_string(std::string_view name)
     constexpr std::size_t size = enum_size<EnumT>();
     for (std::size_t i = 0; i < size; ++i)
         if (names[i] == name)
-            return from_index<EnumT>(i);
+            return enum_enumers<EnumT>()[i];
     throw std::out_of_range("enum index is out of range");
-}
-
-template <enumeration EnumT>
-constexpr EnumT from_value(underlying_type_t<EnumT> value)
-{
-    constexpr const auto& values = enum_values<EnumT>();
-    constexpr std::size_t size = enum_size<EnumT>();
-    for (std::size_t i = 0; i < size; ++i)
-        if (values[i] == value)
-            return static_cast<EnumT>(value);
-    throw std::out_of_range("enum index is out of range");
-}
-
-template <enumeration EnumT>
-constexpr std::optional<EnumT> from_index_opt(std::size_t index) noexcept
-{
-    if (index < enum_size<EnumT>())
-        return enum_array<EnumT>()[index];
-    return {};
-}
-
-template <enumeration EnumT>
-constexpr std::optional<EnumT> from_string_opt(std::string_view name) noexcept
-{
-    constexpr const auto& names = enum_names<EnumT>();
-    constexpr std::size_t size = enum_size<EnumT>();
-    for (std::size_t i = 0; i < size; ++i)
-        if (names[i] == name)
-            return from_index<EnumT>(i);
-    return {};
-}
-
-template <enumeration EnumT>
-constexpr std::optional<EnumT> from_value_opt(underlying_type_t<EnumT> value) noexcept
-{
-    constexpr const auto& values = enum_values<EnumT>();
-    constexpr std::size_t size = enum_size<EnumT>();
-    for (std::size_t i = 0; i < size; ++i)
-        if (values[i] == value)
-            return static_cast<EnumT>(value);
-    return {};
 }
 
 }
